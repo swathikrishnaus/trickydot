e7f80bb98fcee1ff5b187048fed5d953589f6f6c
swathikrishnaus <swathikrisnaus2015@gmail.com>
2022-08-24 18:39:56 +0530

    more files added

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var $grid = $('#portfolio-works').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  }); 
+
+
+
+
+
+
+
+
+  
