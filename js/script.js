1ec53154330594a50d8c15e39f53812d929f91af
swathikrishnaus <swathikrisnaus2015@gmail.com>
2022-09-10 19:48:40 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -7,11 +7,33 @@ var $grid = $('#portfolio-works').isotope({
     $grid.isotope({ filter: filterValue });
   }); 
 
+$(document).scroll(function () {
+    if($(this).scrollTop() > 50){
+      $(".navbar").addClass("navbar-shadow");
+    }else{
+      $(".navbar").removeClass("navbar-shadow");
+    }
+})
 
 
 
 
 
 
+jQuery(document).ready(function() {
 
-  
+  var mouseX = 0, mouseY = 0;
+  var xp = 0, yp = 0;
+   
+  $(document).mousemove(function(e){
+    mouseX = e.pageX - 7;
+    mouseY = e.pageY - 7; 
+  });
+    
+  setInterval(function(){
+    xp += ((mouseX - xp)/6);
+    yp += ((mouseY - yp)/6);
+    $("#circle").css({left: xp +'px', top: yp +'px'});
+  }, 20);
+
+});
